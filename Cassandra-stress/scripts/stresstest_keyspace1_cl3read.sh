3c78ec2bc983381cf38e16b9d1decead2293c50a
noelleheerink <36532032+noelleheerink@users.noreply.github.com>
2019-02-01 20:08:38 +0100

    Update stresstest_keyspace1_cl3read.sh

--- Cassandra-stress/scripts/stresstest_keyspace1_cl3read.sh
+++ Cassandra-stress/scripts/stresstest_keyspace1_cl3read.sh
@@ -3,7 +3,7 @@
 date
 echo
 dstat 1 100 > /tmp/testdstat &
-cassandra-stress read n=1000000 cl=three -rate threads=50  -mode native cql3 user=infra_admin password='jP(C7qVdH!w1hN$B' -schema keyspace="keyspace1" -node `hostname` -graph file=/tmp/stresstestread50thrds.html title=stresstestread50thrds revision=stresstest_`date +%Y%m%d_%H%M%S`_`hostname -s` -log file=/tmp/cassandra_stress_test_logfile_`date +%Y%m%d_%H%M%S`_`hostname -s`
+cassandra-stress read n=1000000 cl=three -rate threads=50  -mode native cql3 user=cassandra password=cassandra -schema keyspace="keyspace1" -node `hostname` -graph file=/tmp/stresstestread50thrds.html title=stresstestread50thrds revision=stresstest_`date +%Y%m%d_%H%M%S`_`hostname -s` -log file=/tmp/cassandra_stress_test_logfile_`date +%Y%m%d_%H%M%S`_`hostname -s`
 echo
 date
 
