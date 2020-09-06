79a527991c1668783d58dac39784b13bf6089c9e
noelleheerink <36532032+noelleheerink@users.noreply.github.com>
2020-09-06 14:14:44 +0200

    Update installchrome.ps1

--- installchrome.ps1
+++ installchrome.ps1
@@ -1,1 +1,3 @@
-$LocalTempDir = $env:TEMP; $ChromeInstaller = "ChromeInstaller.exe"; (new-object System.Net.WebClient).DownloadFile('https://dl.google.com/chrome/install/375.126/chrome_installer.exe', "$LocalTempDir\$ChromeInstaller"); & "$LocalTempDir\$ChromeInstaller" /silent /install; $Process2Monitor = "ChromeInstaller"; Do { $ProcessesFound = Get-Process | ?{$Process2Monitor -contains $_.Name} | Select-Object -ExpandProperty Name; If ($ProcessesFound) { "Still running: $($ProcessesFound -join ', ')" | Write-Host; Start-Sleep -Seconds 2 } else { rm "$LocalTempDir\$ChromeInstaller" -ErrorAction SilentlyContinue -Verbose } } Until (!$ProcessesFound)
+$uri = "https://dl.google.com/chrome/install/latest/chrome_installer.exe" 
+$path = "$PSScriptRoot\ChromeSetup.exe" 
+Invoke-WebRequest -Uri $uri -OutFile $path Start-Process $path /install -NoNewWindow -Wait Remove-Item .\ChromeSetup.exe
